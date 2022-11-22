77546435aca2fbb34f9d78c2e3eadc5e8ea6a610
zahyaah <itszayd2003@gmail.com>
2022-11-22 20:11:50 +0530

    Added templates

--- app1/urls.py
+++ app1/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index)
+    path('index/', views.index),
+    path('index1/', views.index1),
 ]
--- app1/views.py
+++ app1/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Hello World</h1>")
+
+
+def index1(request):
+    return render(request, 'app1/first.html',{})
