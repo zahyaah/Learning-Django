182eaecce9ef36041dbc994add4b3e84c5e466e9
zahyaah <itszayd2003@gmail.com>
2022-11-22 22:23:38 +0530

    Created model Remark

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Remark(models.Model):
+    name = models.CharField(max_length=100)
+    text = models.TextField(blank=False)  # the field cannot be blank
+
+    def __str__(self):
+        return self.name
