39196c19aa0edf042e0d368a407bee9652448a29
zahyaah <itszayd2003@gmail.com>
2022-11-23 13:48:51 +0530

    Class based view index1 and index2

--- app1/urls.py
+++ app1/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index),
-    path('index1/', views.index1),
-    path('index2/', views.index2),
+    path('index1/', views.Index1.as_view()),
+    path('index2/', views.Index2.as_view()),
     path('index3/<int:pk>', views.index3),
+    path('index4/', views.index4)
 ]
--- app1/views.py
+++ app1/views.py
@@ -2,21 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import date
 from .models import Remark
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
 
 
 def index(request):
     return HttpResponse("<h1>Hello World</h1>")
 
 
-def index1(request):
-    return render(request, 'app1/first.html', {"date": date.today()})
+# def index1(request):
+#     return render(request, 'app1/first.html', {"date": date.today()})
+class Index1(TemplateView):
+    template_name = 'app1/first.html'
+    extra_context = {"date": date.today()}
 
 
-def index2(request):
+# def index2(request):
+#     dataStored = Remark.objects.all()
+#     return render(request, 'app1/AccessIt.html', {"passData": dataStored})
+class Index2(TemplateView):
     dataStored = Remark.objects.all()
-    return render(request, 'app1/AccessIt.html', {"passData": dataStored})
+    template_name = 'app1/AccessIt.html'
+    extra_context = {"passData": dataStored}
 
 
 def index3(request, pk):
     dateStored = Remark.objects.get(pk=pk)
     return render(request, 'app1/priKey.html', {"ds": dateStored})
+
+
+@login_required(redirect_field_name="http://127.0.0.1:8000/index4", login_url='http://127.0.0.1:8000/admin')
+def index4(request):
+    return render(request, 'app1/restriction.html', {})
